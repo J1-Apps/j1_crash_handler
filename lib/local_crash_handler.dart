92ad6bf43f3f3ce9f85e85e7d0a24a52486afc36
jaewon <JMichaelYang@users.noreply.github.com>
2024-08-25 15:13:36 -0400

    #1: remove J1 logger dependency (#2)

--- lib/local_crash_handler.dart
+++ lib/local_crash_handler.dart
@@ -1,18 +1,18 @@
 import "package:flutter/material.dart";
 import "package:j1_crash_handler/j1_crash_handler.dart";
-import "package:j1_logger/j1_logger.dart";
+import "dart:developer";
 
 class LocalCrashHandler extends J1CrashHandler {
   const LocalCrashHandler();
 
   @override
   void handleFlutterError(FlutterErrorDetails details) {
-    J1Logger.log(name: "flutter error", params: {"error": details.exception, "stackTrace": details.stack ?? "N/A"});
+    log("flutter error", error: details.exception, stackTrace: details.stack);
   }
 
   @override
   bool handleAsyncError(Object object, StackTrace stack) {
-    J1Logger.log(name: "async error", params: {"error": object, "stackTrace": stack});
+    log("async error", error: object, stackTrace: stack);
     return true;
   }
 }
